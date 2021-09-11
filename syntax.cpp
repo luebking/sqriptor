32562ccf5a941ad8c67d8bf8be2705d31721bb65
Thomas Lübking <thomas.luebking@gmail.com>
2021-09-11 21:09:11 +0200

    constrain markdown lineheight, might be too aggressive

--- syntax.cpp
+++ syntax.cpp
@@ -155,6 +155,17 @@ static void resetColors(QsciScintilla *document, Syntax::Lexer syntax) {
     document->setWhitespaceForegroundColor(fg);
     document->setEdgeMode(syntax == Syntax::Markdown2 ? QsciScintilla::EdgeBackground : QsciScintilla::EdgeLine);
     document->setMarginsFont(Sqriptor::config.font);
+
+    int extents = 0;
+    if (syntax == Syntax::Markdown2) {
+        QFont fnt = Sqriptor::config.font;
+        int hght1 = QFontMetrics(fnt).height();
+        fnt.setPointSize(fnt.pointSize()*1.4);
+        int hght2 = QFontMetrics(fnt).height();
+        extents = hght1 - hght2; // yup - negative
+    }
+    document->setExtraAscent(extents);
+    document->setExtraDescent(extents);
 }
 
 #define TRICK_QSCINTILLA_DETACHLEXER document->setLexer(nullptr); resetColors(document, syntax); document->SendScintilla(QsciScintilla::SCI_STYLECLEARALL);
